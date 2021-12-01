8bf66d0abf38182be0f6e08161499bc4e9a79d67
luisluft <luberluft@gmail.com>
2021-12-01 10:16:14 -0400

    extract route params

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
+import ProductDetail from "./pages/ProductDetail";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/welcome" element={<Welcome />}></Route>
         <Route path="/products" element={<Products />}></Route>
+        <Route path="/products/detail/:productId" element={<ProductDetail />}></Route>
       </Routes>
     </>
   );
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Products = () => {
-  return <h1>The Products Page</h1>;
+  return (
+    <section>
+      <h1>The Products Page</h1>
+      <ul>
+        <li>A book</li>
+        <li>A carpet</li>
+        <li>An online course</li>
+      </ul>
+    </section>
+  );
 };
 
 export default Products;
