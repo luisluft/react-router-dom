7c9feb6aea554ffc8cf557fbb6d34a2759cbf4df
luisluft <luberluft@gmail.com>
2021-12-02 08:57:43 -0400

    add a redirect in root url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Welcome from "./pages/Welcome";
 import Products from "./pages/Products";
 import MainHeader from "./components/MainHeader";
@@ -9,6 +9,7 @@ function App() {
     <>
       <MainHeader />
       <Routes>
+        <Route path="*" element={<Navigate replace to="/welcome" />} />
         <Route path="/welcome" element={<Welcome />}></Route>
         <Route
           path="/welcome/new-user"
